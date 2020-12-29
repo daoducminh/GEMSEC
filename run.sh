05229cf057a1604cb26b886224618cea41162500
Minh Dao <daoducminh1997@gmail.com>
2020-12-29 16:11:06 +0700

    Update run

--- run.sh
+++ run.sh
@@ -8,6 +8,10 @@ mkdir -p out/cluster_means/
 mkdir -p out/logs/
 mkdir -p out/assignment/
 
+# Remove
+rm -rf $1.txt
+rm -rf $1.csv
+
 # Download dataset
 wget -q "https://snap.stanford.edu/data/$1.$GZ" && gunzip -k "$1.$GZ" && rm "$1.$GZ"
 
