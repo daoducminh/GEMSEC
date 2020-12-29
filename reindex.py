e831a0f525e95d31c9004e947e5ecb72ee14bff9
Minh Dao <daoducminh1997@gmail.com>
2020-12-29 16:02:29 +0700

    Add reindex data

---
+++ reindex.py
@@ -0,0 +1,34 @@
+import sys
+import pandas as pd
+import numpy as np
+
+
+def reindex(filename):
+    df = pd.read_csv(filename)
+    rs = set()
+    for _, row in df.iterrows():
+        rs.add(row['node_1'])
+        rs.add(row['node_2'])
+    return rs, df
+
+
+def write_mapping(rs, filename, folder):
+    with open(f'{folder}/{filename}_mapping.csv', 'w') as f:
+        f.write('index,node\n')
+        for index, value in enumerate(rs):
+            f.write(f'{index},{value}\n')
+
+
+def replace_df(rs, df):
+    for index, value in enumerate(rs):
+        df.replace(value, index, inplace=True)
+    df.to_csv(filename, index=False)
+    df.to_csv(f'out/{filename}', index=False)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        rs, df = reindex(filename)
+        write_mapping(rs, filename, 'out')
+        replace_df(rs, df)
--- run_dw.sh
+++ run_dw.sh
@@ -18,6 +18,9 @@ sed -i 's/ /,/g' $1.txt
 echo -e 'node_1,node_2' | cat - $1.txt > tmp
 mv tmp $1.csv
 
+# Reindex data
+.venv/bin/python reindex.py $1.csv
+
 # Run embedding
 .venv/bin/python src/embedding_clustering.py --model DeepWalk --dimensions 32 --num-of-walks 5 --random-walk-length 80 --cluster-number 10 --input $1.csv  --embedding-output out/embeddings/$1.csv --log-output out/cluster_means/$1.csv --cluster-mean-output out/logs/$1.csv --assignment-output out/assignment/$1.json
 
