5015a32d2c44210c4d71319e07cbd3dc45344207
Minh Dao <daoducminh1997@gmail.com>
2020-12-29 14:29:04 +0700

    Add run_dw.sh

---
+++ run_dw.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GZ="txt.gz"
+
+# Make all directories
+mkdir -p out/embeddings/
+mkdir -p out/cluster_means/
+mkdir -p out/logs/
+mkdir -p out/assignment/
+
+# Download dataset
+wget -q "https://snap.stanford.edu/data/$1.$GZ" && gunzip -k "$1.$GZ" && rm "$1.$GZ"
+
+# Replace whitespace with comma
+sed -i 's/ /,/g' $1.txt
+
+# Add header
+echo -e 'node_1,node_2' | cat - $1.txt > tmp
+mv tmp $1.csv
+
+# Run embedding
+.venv/bin/python src/embedding_clustering.py --model DeepWalk --dimensions 32 --num-of-walks 20 --random-walk-length 160 --cluster-number 10 --input $1.csv  --embedding-output out/embeddings/$1.csv --log-output out/cluster_means/$1.csv --cluster-mean-output out/logs/$1.csv --assignment-output out/assignment/$1.json
+
+# Save result to gdrive
+cp -rf out/ /content/gdrive/My\ Drive/Projects/
