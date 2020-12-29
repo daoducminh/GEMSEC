7410be08e97378324db2c6b6ce73e4d3ddbb8d3a
Minh Dao <daoducminh1997@gmail.com>
2020-12-29 20:50:25 +0700

    Update all run

--- run_r.sh
+++ run_r.sh
@@ -27,7 +27,7 @@ mv tmp $1.csv
 .venv/bin/python reindex.py $1.csv
 
 # Run embedding
-.venv/bin/python src/embedding_clustering.py --model GEMSEC --dimensions 32 --num-of-walks 5 --random-walk-length 80 --cluster-number 10 --input $1.csv  --embedding-output out/embeddings/$1.csv --log-output out/cluster_means/$1.csv --cluster-mean-output out/logs/$1.csv --assignment-output out/assignment/$1.json
+.venv/bin/python src/embedding_clustering.py --model GEMSECWithRegularization --dimensions 32 --num-of-walks 5 --random-walk-length 80 --cluster-number 10 --input $1.csv  --embedding-output out/embeddings/$1.csv --log-output out/cluster_means/$1.csv --cluster-mean-output out/logs/$1.csv --assignment-output out/assignment/$1.json
 
 # Save result to gdrive
 cp -rf out/ /content/gdrive/My\ Drive/Projects/
