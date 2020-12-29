26569142a179fe70a23b795815850dc0425f8713
Minh Dao <daoducminh1997@gmail.com>
2020-12-29 16:19:40 +0700

    Update reindex

--- reindex.py
+++ reindex.py
@@ -13,17 +13,17 @@ def reindex(filename):
 
 
 def write_mapping(rs, filename, folder):
-    with open(f'{folder}/mapping/{filename}', 'w') as f:
+    with open('{0}/mapping/{1}'.format(folder, filename), 'w') as f:
         f.write('index,node\n')
         for index, value in enumerate(rs):
-            f.write(f'{index},{value}\n')
+            f.write('{0},{1}\n'.format(index, value))
 
 
 def replace_df(rs, df):
     for index, value in enumerate(rs):
         df.replace(value, index, inplace=True)
     df.to_csv(filename, index=False)
-    df.to_csv(f'out/{filename}', index=False)
+    df.to_csv('out/{0}'.format(filename), index=False)
 
 
 if __name__ == "__main__":
